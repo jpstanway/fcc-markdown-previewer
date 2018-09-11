f0f0b6b6639f6c9cb68f6fd94de6c555cd23f878
Jordan S <jpstanway@gmail.com>
2018-09-11 16:50:55 -0700

    toggle legend  icon between plus and minus on click

--- app/src/App.js
+++ app/src/App.js
@@ -3,23 +3,39 @@ import Preview from './modules/preview';
 import Editor from './modules/editor';
 import Legend from './modules/legend';
 import defaultText from './modules/default-text';
+import ICONS from './modules/icons';
 import './stylesheets/App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      previewText: defaultText
+      previewText: defaultText,
+      icons: ICONS,
+      collapsed: "true"
     }
     this.handleChange = this.handleChange.bind(this);
+    this.getIcon = this.getIcon.bind(this);
+    this.collapse = this.collapse.bind(this);
   }
 
   handleChange(event) {
     const text = event.target.value;
-
+    
     this.setState({
       previewText: text
     })
+  }
+
+  getIcon() {
+    return this.state.icons[this.state.collapsed];
+  }
+
+  collapse() {
+    const collapse = this.state.collapsed === "true" ? "false" : "true";
+    this.setState({
+      collapsed: collapse
+    });
   }
 
   render() {
@@ -28,7 +44,7 @@ class App extends Component {
     return(
       <div id="container">
         <Preview text={previewText} />
-        <Legend />
+        <Legend icon={this.getIcon} collapse={this.collapse} />
         <Editor val={previewText} handler={this.handleChange} />
       </div>
     );
--- app/src/modules/legend.js
+++ app/src/modules/legend.js
@@ -1,26 +1,60 @@
 import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const Legend = (props) => {
     return(
         <fieldset id="legend">
-            <legend className="char">Legend</legend>
+            <legend id="legend-collapse" className="char" onClick={props.collapse}>Legend
+                <TransitionGroup id="transition-div">
+                    <CSSTransition
+                        key={props.collapse}
+                        timeout={500}
+                        classNames="collapse"
+                    >
+                        {props.icon} 
+                    </CSSTransition>
+                </TransitionGroup>
+            </legend>
             <div id="list-container">
                 <ul className="legend-list">
-                    <li><span className="char">#</span> for <span className="element">h1</span></li>
-                    <li><span className="char">##</span> for <span className="element">h2</span></li>
-                    <li><span className="char">###</span> for <span className="element">h2</span></li>
-                    <li><span className="char">[link text](url)</span> for <span className="element">links</span></li>
+                    <li><span className="char">#</span> for 
+                        <span className="element"> h1</span>
+                    </li>
+                    <li><span className="char">##</span> for 
+                        <span className="element">h2</span>
+                    </li>
+                    <li><span className="char">###</span> for 
+                        <span className="element"> h2</span>
+                    </li>
+                    <li><span className="char">[link text](url)</span> for 
+                        <span className="element"> links</span>
+                    </li>
                 </ul>
                 <ul className="legend-list">
-                    <li><span className="char">`&lt;div&gt;&lt;/div&gt;`</span> for <span className="element">inline code</span></li>
-                    <li><span className="char">```&lt;div&gt;&lt;/div&gt;```</span> for <span className="element">multi-line code</span></li>
-                    <li><span className="char">- list item</span> for <span className="element">lists</span></li>
-                    <li><span className="char">> text</span> for <span className="element">blockquotes</span></li>
+                    <li><span className="char">`&lt;div&gt;&lt;/div&gt;`</span> for 
+                        <span className="element"> inline code</span>
+                    </li>
+                    <li><span className="char">```&lt;div&gt;&lt;/div&gt;```</span> for 
+                        <span className="element"> multi-line code</span>
+                    </li>
+                    <li><span className="char">- list item</span> for 
+                        <span className="element"> lists</span>
+                    </li>
+                    <li><span className="char">> text</span> for 
+                        <span className="element"> blockquotes</span>
+                    </li>
                 </ul>
                 <ul className="legend-list">
-                    <li><span className="char">![img text](url)</span> for <span className="element">images</span></li>
-                    <li><span className="char">**text**</span> for <span className="element">bold text</span></li>
-                    <li className="element">for more options check out the <a href="https://github.github.com/gfm/" target="_blank" rel="noopener noreferrer">gfm</a> docs</li>
+                    <li><span className="char">![img text](url)</span> for 
+                        <span className="element"> images</span>
+                    </li>
+                    <li><span className="char">**text**</span> for 
+                        <span className="element"> bold text</span>
+                    </li>
+                    <li className="element">for more options check out the <a 
+                        href="https://github.github.com/gfm/" target="_blank" 
+                        rel="noopener noreferrer">gfm</a> docs
+                    </li>
                 </ul>
             </div>
         </fieldset>
