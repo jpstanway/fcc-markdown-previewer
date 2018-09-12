caef0abc937b7331371eb4588cb3caa05aba0179
Jordan S <jpstanway@gmail.com>
2018-09-12 14:31:26 -0700

    make links open in new windows by default using Marked renderer method

--- app/src/modules/preview.js
+++ app/src/modules/preview.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Marked from 'marked';
 
+// set GitHub Flavored Markdown to true in Marked options
 Marked.setOptions({
   gfm: true
 });
 
+// assign target="_blank" to anchor elements via Marked renderer method
+let renderer = new Marked.Renderer();
+
+renderer.link = function(href, title, text) {
+  return `<a href="${href}" target="_blank">${text}</a>`;
+}
+
+// Preview component rendered with Marked
 const Preview = (props) => {
   return(
-    <div id="preview" dangerouslySetInnerHTML={{__html: Marked(props.text)}}></div>
+    <div id="preview" dangerouslySetInnerHTML={{__html: Marked(props.text, { renderer: renderer })}}></div>
   );
 };
     
